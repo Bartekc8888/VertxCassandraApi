823d46a0eb05a038ddb18c53395288b6a6705a2d
Bartłomiej Ciesielski <bartekc8@op.pl>
2019-04-13 16:53:02 +0200

    Init Vert.x rest api project.

---
+++ src/main/java/com/politechnika/Main.java
@@ -0,0 +1,22 @@
+package com.politechnika;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+
+public class Main extends AbstractVerticle {
+    @Override
+    public void start(Future<Void> future) {
+        vertx.createHttpServer().requestHandler(r ->
+            r.response()
+             .end("<h1>Hello from " +
+                  "Vert.x 3 application</h1>")
+        ).listen(8080, result -> {
+            if (result.succeeded()) {
+                future.complete();
+            } else {
+                future.fail(result.cause());
+            }
+        });
+    }
+}
